fa69b239426d5d188fabd65cc4e184265853f157
JonathanTGold <jonathan.gold@86@gmail.com>
2022-05-17 13:43:26 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,60 @@
 const path = require('path');
 
-const config = {
-  target: 'web',
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss/,
-        use: [
-          'style-loader',
-          {
-            loader: "css-loader",
-            options: {
-              esModule: true,
-              modules: {
-                namedExport: true,
+module.exports = (env, { mode }) => {
+  return {
+    target: 'web',
+    entry: './src/index.ts',
+    devtool: mode === 'development' ? 'source-map' : '',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.scss/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                esModule: true,
+                modules: {
+                  localIdentName: '[local]_[hash:base64:5]',
+                  namedExport: true,
+                },
               },
             },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: mode === 'development',
+              },
             },
-          },
-        ],
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: 'ui-manager.js',
+      path: path.resolve(__dirname, 'build'),
+      clean: true,
+    },
+    externals: {
+      'kaltura-player-js': 'root KalturaPlayer',
+      preact: 'root KalturaPlayer.ui.preact',
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'demo'),
       },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'ui-manager.js',
-    path: path.resolve(__dirname, 'build'),
-    clean: true,
-  },
-  externals: {
-    'kaltura-player-js': 'root KalturaPlayer',
-    preact: 'root KalturaPlayer.ui.preact',
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'demo'),
+      client: {
+        progress: true,
+      },
     },
-    client: {
-      progress: true,
-    },
-  },
-};
-
-module.exports = (env, { mode }) => {
-  if (mode === 'development') {
-    config.devtool = 'source-map';
-  }
-  return config;
+  };
 };
