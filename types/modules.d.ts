ff35945247954582b53f93878f3899eef0301260
JonathanTGold <jonathan.gold@86@gmail.com>
2022-05-30 11:19:53 +0300

    types generate

--- types/modules.d.ts
+++ types/modules.d.ts
@@ -1,10 +1,14 @@
 declare module "services/side-panels/models/side-panel-item-dto" {
     import { ComponentClass, FunctionalComponent } from 'preact';
     import { PlaykitUI } from 'kaltura-player-js';
+    export type PanelComponentProps = {
+        isActive: boolean;
+    };
+    export type IconComponentProps = PanelComponentProps;
     export interface SidePanelItemDto {
         readonly label: string;
-        readonly renderIcon?: ComponentClass | FunctionalComponent;
-        readonly renderContent: ComponentClass | FunctionalComponent;
+        readonly iconComponent?: ComponentClass<IconComponentProps> | FunctionalComponent<IconComponentProps>;
+        readonly panelComponent: ComponentClass<PanelComponentProps> | FunctionalComponent<PanelComponentProps>;
         readonly presets: PlaykitUI.ReservedPresetName[];
         readonly position: PlaykitUI.SidePanelPosition;
         readonly expandMode: PlaykitUI.SidePanelMode;
@@ -13,8 +17,8 @@ declare module "services/side-panels/models/side-panel-item-dto" {
     }
     export class SidePanelItem implements SidePanelItemDto {
         readonly label: string;
-        readonly renderIcon?: ComponentClass | FunctionalComponent;
-        readonly renderContent: ComponentClass | FunctionalComponent;
+        readonly iconComponent?: ComponentClass<IconComponentProps> | FunctionalComponent<IconComponentProps>;
+        readonly panelComponent: ComponentClass<PanelComponentProps> | FunctionalComponent<PanelComponentProps>;
         readonly presets: PlaykitUI.ReservedPresetName[];
         readonly position: PlaykitUI.SidePanelPosition;
         readonly expandMode: PlaykitUI.SidePanelMode;
@@ -23,7 +27,8 @@ declare module "services/side-panels/models/side-panel-item-dto" {
         constructor(item: SidePanelItemDto);
     }
 }
-declare module "services/side-panels/ui/toggel" { }
+declare module "services/side-panels/ui/panel-item-wrapper/panel-item-wrapper.component" { }
+declare module "services/side-panels/ui/icon-wrapper/icon-wrapper.component" { }
 declare module "services/side-panels/models/item-wrapper" { }
 declare module "services/side-panels/side-panels-manager" {
     import { KalturaPlayer, Logger } from 'kaltura-player-js';
@@ -39,6 +44,7 @@ declare module "services/side-panels/side-panels-manager" {
         activateItem(itemId: number): void;
         deactivateItem(itemId: number, switchMode?: boolean): void;
         isItemActive(itemId: number): boolean;
+        update(itemId: number): void;
         reset(): void;
         private toggle;
         private expand;
