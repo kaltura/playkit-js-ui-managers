8ff26d915d232cbfb6d4d9c74e4ac54c3c4923a0
JonathanTGold <jonathan.gold@86@gmail.com>
2022-05-31 16:36:19 +0300

    types generate

--- types/modules.d.ts
+++ types/modules.d.ts
@@ -5,7 +5,7 @@ declare module "services/side-panels/models/side-panel-item-dto" {
         isActive: boolean;
     };
     export type IconComponentProps = PanelComponentProps;
-    export interface SidePanelItemDto {
+    export interface SidePanelItem {
         readonly label: string;
         readonly iconComponent?: ComponentClass<IconComponentProps> | FunctionalComponent<IconComponentProps>;
         readonly panelComponent: ComponentClass<PanelComponentProps> | FunctionalComponent<PanelComponentProps>;
@@ -14,17 +14,6 @@ declare module "services/side-panels/models/side-panel-item-dto" {
         readonly expandMode: PlaykitUI.SidePanelMode;
         readonly onActivate?: () => void;
         readonly onDeactivate?: () => void;
-    }
-    export class SidePanelItem implements SidePanelItemDto {
-        readonly label: string;
-        readonly iconComponent?: ComponentClass<IconComponentProps> | FunctionalComponent<IconComponentProps>;
-        readonly panelComponent: ComponentClass<PanelComponentProps> | FunctionalComponent<PanelComponentProps>;
-        readonly presets: PlaykitUI.ReservedPresetName[];
-        readonly position: PlaykitUI.SidePanelPosition;
-        readonly expandMode: PlaykitUI.SidePanelMode;
-        readonly onActivate?: () => void;
-        readonly onDeactivate?: () => void;
-        constructor(item: SidePanelItemDto);
     }
 }
 declare module "services/side-panels/ui/panel-item-wrapper/panel-item-wrapper.component" { }
@@ -32,26 +21,24 @@ declare module "services/side-panels/ui/icon-wrapper/icon-wrapper.component" { }
 declare module "services/side-panels/models/item-wrapper" { }
 declare module "services/side-panels/side-panels-manager" {
     import { KalturaPlayer, Logger } from 'kaltura-player-js';
-    import { SidePanelItemDto } from "services/side-panels/models/side-panel-item-dto";
+    import { SidePanelItem } from "services/side-panels/models/side-panel-item-dto";
     export class SidePanelsManager {
         private readonly player;
         private readonly activePanels;
         private readonly componentsRegistry;
         private readonly logger;
         constructor(player: KalturaPlayer, logger: Logger);
-        addItem(item: SidePanelItemDto): number | void;
+        addItem(item: SidePanelItem): number | void;
         removeItem(itemId: number): void;
         activateItem(itemId: number): void;
-        deactivateItem(itemId: number, switchMode?: boolean): void;
+        deactivateItem(itemId: number): void;
+        private _deactivateItem;
         isItemActive(itemId: number): boolean;
         update(itemId: number): void;
         reset(): void;
         private toggle;
         private expand;
         private collapse;
-        private injectIconComponent;
-        private injectPanelComponent;
-        private static getPanelArea;
         private static getOppositePanelPosition;
         private static validateItem;
     }
