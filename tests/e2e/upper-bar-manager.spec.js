2a15cadd70ddf7b4c7117682222a235e31b713d6
JonathanTGold <jonathan.gold@86@gmail.com>
2022-09-25 12:26:04 +0200

    add update api unit test

--- tests/e2e/upper-bar-manager.spec.js
+++ tests/e2e/upper-bar-manager.spec.js
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import { createSandbox, spy } from 'sinon';
 import '../../src/index';
 import { setup } from 'kaltura-player-js';
 import { IconComponent } from '../mock/compenents/icon.component';
@@ -6,25 +7,27 @@ import { config, targetId } from '../mock/config';
 import { mediaData } from '../mock/media-sourc';
 import '../mock/plugins/index';
 
-describe('Upper Bar Manager', function () {
+describe('Upper Bar Manager', () => {
   let player;
   let upperBarManagerService;
+  const sandbox = createSandbox();
 
-  before(function () {
+  before(() => {
     const element = document.createElement('DIV');
     element.id = targetId;
     document.body.appendChild(element);
   });
 
-  after(function () {
+  after(() => {
     document.getElementById(targetId).remove();
   });
 
-  afterEach(function () {
+  afterEach(() => {
     player.destroy();
-    for (let element of document.getElementsByTagName('video')) {
+    for (const element of document.getElementsByTagName('video')) {
       element.remove();
     }
+    sandbox.restore();
   });
 
   it('Add icon', async () => {
@@ -72,6 +75,33 @@ describe('Upper Bar Manager', function () {
     // Expect
     expect(upperBarManagerService.isActive(iconId1)).to.be.false;
     expect(upperBarManagerService.isActive(iconId2)).to.be.true;
+  });
+
+  it('Update Icon', (done) => {
+    // Given
+    player = setup(config);
+    upperBarManagerService = player.getService('upperBarManager');
+    player.setMedia({ sources: { ...mediaData } });
+
+    // Do
+    player.ready().then(() => {
+      const icon1Id = upperBarManagerService.add({ label: 'icon1', component: IconComponent, onClick: () => {} });
+      const icon2Id = upperBarManagerService.add({ label: 'icon2', component: IconComponent, onClick: () => {} });
+      setTimeout(() => {
+        const iconModel1 = upperBarManagerService.componentsRegistry.get(icon1Id);
+        const iconModel2 = upperBarManagerService.componentsRegistry.get(icon2Id);
+        sandbox.spy(iconModel1.componentRef.current, 'render');
+        sandbox.spy(iconModel2.componentRef.current, 'render');
+        upperBarManagerService.update(icon1Id);
+
+        // Expect
+        setTimeout(() => {
+          expect(iconModel1.componentRef.current.render.calledOnce).to.be.true;
+          expect(iconModel2.componentRef.current.render.calledOnce).to.be.false;
+        });
+        done();
+      });
+    });
   });
 
   it('icons order state compatible with the configuration', (done) => {
@@ -144,14 +174,24 @@ describe('Upper Bar Manager', function () {
 
     player.ready().then(() => {
       setTimeout(() => {
-        const previousComponentControlsState = JSON.parse(JSON.stringify(upperBarManagerService.displayedBarComponentRef.current.state.controls.map(({id, label}) => ({id, label}))));
-        const previousServiceRegistryState = JSON.parse(JSON.stringify(Array.from(upperBarManagerService.componentsRegistry.values()).map(({id, label}) => ({id, label}))));
+        const previousComponentControlsState = JSON.parse(
+          JSON.stringify(
+            upperBarManagerService.displayedBarComponentRef.current.state.controls.map(({ id, label }) => ({ id, label }))
+          )
+        );
+        const previousServiceRegistryState = JSON.parse(
+          JSON.stringify(Array.from(upperBarManagerService.componentsRegistry.values()).map(({ id, label }) => ({ id, label })))
+        );
 
         player.setMedia({ sources: { ...mediaData } });
 
         setTimeout(() => {
-          const currentComponentControlsState = upperBarManagerService.displayedBarComponentRef.current.state.controls.map(({id, label}) => ({id, label}));
-          const currentServiceRegistryState = Array.from(upperBarManagerService.componentsRegistry.values()).map(({id, label}) => ({id, label}));
+          const currentComponentControlsState = upperBarManagerService.displayedBarComponentRef.current.state.controls.map(
+            ({ id, label }) => ({ id, label })
+          );
+          const currentServiceRegistryState = Array.from(upperBarManagerService.componentsRegistry.values()).map(
+            ({ id, label }) => ({ id, label })
+          );
           expect(previousComponentControlsState).to.be.deep.equal(currentComponentControlsState);
           expect(previousServiceRegistryState).to.be.deep.equal(currentServiceRegistryState);
           done();
