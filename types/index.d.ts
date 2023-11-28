739ec62ea1878dd5f0a8884d8acb99c1397c7a61
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2023-11-28 16:17:55 +0200

    export toastmanager and bannermanager

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,6 +3,8 @@
 export { FloatingManager } from "services/floating-manager/floating-manager";
 export { FloatingItem } from "services/floating-manager/ui/floating-item";
 export { FloatingItemProps } from "services/floating-manager/models/floating-item-data";
+export { ToastManager } from 'services/toast-manager/toast-manager';
+export { BannerManager } from 'services/banner-manager/banner-manager';
 
 export { SidePanelsManager } from "services/side-panels-manager/side-panels-manager";
 export { UpperBarManager } from "services/upper-bar-manager/upper-bar-manager";
